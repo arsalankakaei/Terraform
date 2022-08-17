b503403de54023463614e980ba427af2a5f5619d
kakaei <kakaei@domain.bpp.com>
2022-08-17 08:22:16 +0430

    My first successful terraform commit that build a nginix container!!!

--- main.tf
+++ main.tf
@@ -1,19 +1,18 @@
+# Create a docker image resource
+# -> docker pull nginx:latest
 resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
+  name = "nginx:latest"
+  keep_locally = true
 }
 
+# Create a docker container resource
+# -> same as 'docker run --name nginx -p8080:80 -d nginx:latest'
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
-  }
+  name    = "nginx"
+  image   = docker_image.nginx.latest
 
-  volumes {
-    container_path = "/usr/share/nginx/html"
-    host_path      = "/data"
-    read_only      = true
+  ports {
+    external = 8080
+    internal = 80
   }
 }
